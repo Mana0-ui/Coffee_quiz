215de34d48eb742c896a85d6f5b993d61e7a4238
mana <mananico0319@gmail.comgit config --global user.email mananico0319@gmail.com>
2020-08-13 20:01:13 +0900

    Linear Layoutへの変更
    Q1
    Question
    回答ボタンの作成
    
    クイズの選択肢の配列を作成
    クイズを表示して、正解、不正解をアラートで表示

--- app/src/main/java/com/example/coffee_quiz/MainActivity.java
+++ app/src/main/java/com/example/coffee_quiz/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.coffee_quiz;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private TextView countLabel;
@@ -19,8 +24,10 @@ public class MainActivity extends AppCompatActivity {
     private String rightAnswer; //回答を入れるための変数
     private int rightAnswerCount = 0; //正解を入れるための変数
     private int quizCount = 1; //何問目を出題しているのか数える
+    static final private int QUIZ_COUNT = 5;//5回問題を出題する
 
     ArrayList<ArrayList<String>> quizArray = new ArrayList<>();
+    ArrayList<String> test = new ArrayList<>();
 
     String quizData[][] = {
             //{"コーヒー名","正解","選択肢１","選択肢２","選択肢３"}
@@ -64,6 +71,58 @@ public class MainActivity extends AppCompatActivity {
 
             quizArray.add(tmpArray);
         }
+        showNextQuiz();
+    }
+    public void showNextQuiz(){
+        //何問目かを表示する
+        countLabel.setText("Q" + quizCount);
+
+        //ランダムな数字を取得
+        Random random = new Random();
+        int randomNum = random.nextInt(quizArray.size());//quizArrayの数まででランダムに数を作成
+        ArrayList<String> quiz = quizArray.get(randomNum);
+
+        questionLabel.setText(quiz.get(0));//[0]が問題
+        rightAnswer =quiz.get(1);//[1]が解答
+        quiz.remove(0);//クイズから問題の配列を削除
+        Collections.shuffle(quiz);//選択肢と解答をシャッフル
+
+        answerBtn1.setText(quiz.get(0));//ボタン1〜4に解答、選択肢を表示
+        answerBtn2.setText(quiz.get(1));
+        answerBtn3.setText(quiz.get(2));
+        answerBtn4.setText(quiz.get(3));
+
+        quizArray.remove(randomNum);//この問題をquizArrayから削除
 
     }
+
+    public void checkAnswer(View view){
+        Button answerBtn = findViewById(view.getId());//押された解答ボタンを見つける
+        String btnText = answerBtn.getText().toString();//そのテキストを取得する
+        String alertTitle;
+        if(btnText.equals(rightAnswer)){ //回答ボタンのテキストと、rightAnswerが一致するかどうかで判定
+            alertTitle = "正解！";
+            rightAnswerCount++;
+        }else{
+            alertTitle = "不正解！";
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(alertTitle);
+        builder.setMessage("答え："+rightAnswer);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(quizCount ==QUIZ_COUNT){
+                    //結果画面に移動する
+                }else{
+                    quizCount++;
+                    showNextQuiz();
+                }
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
+
 }
